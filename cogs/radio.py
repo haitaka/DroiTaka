c5487658ac6af08186d0b5b84481a1a26ecc3651
Haitaka <haitaka@users.noreply.github.com>
2016-02-14 18:13:57 +0000

    list add current and update command return

--- cogs/radio.py
+++ cogs/radio.py
@@ -42,60 +42,53 @@ class Radio:
     
     @commands.command()
     async def join(self, *, channel : discord.Channel = None):
-        """Join voice channel.
-        """
+        """Зайти на указанный голосовой канал."""
         if channel is None or channel.type is not discord.ChannelType.voice:
-            await self.bot.say('Cannot find a voice channel by that name. {0}'.format(channel.type))
+            await self.bot.say('Нет такого голосового канала.')
             return
         await self.bot.join_voice_channel(channel)
         
     @commands.command(pass_context=True)
     async def leave(self, ctx):
-        """Leave voice channel.
-        """
+        """Покинуть текущий голосовой каналю"""
         await ctx.invoke(self.stop)
         await self.bot.voice.disconnect()
         
     @commands.command()
     async def pause(self):
-        """Pause.
-        """
+        """Приостановить воспроизведение."""
         if self.player is not None:
             self.player.pause()
             
     @commands.command()
     async def resume(self):
-        """Resume playing.
-        """
+        """Продолжить воспроизведение."""
         if self.player is not None and not self.is_playing:
             self.player.resume()
             
     @commands.command()
     async def skip(self):
-        """Skip song and play next.
-        """
+        """Перейти к следующей песне в очереди."""
         if self.player is not None and self.is_playing:
             self.player.stop()
             self.toggle_next_song()
             
     @commands.command()
     async def stop(self):
-        """Stop playing song.
-        """
+        """Остановить воспроизведение."""
         if self.is_playing:
             self.stopped = True
             self.player.stop()
 
     @commands.command(pass_context=True)
     async def play(self, ctx):
-        """Start playing song from queue.
-        """
+        """Начать воспроизведение песен из очереди."""
         if self.player is not None and not self.stopped:
             if not self.is_playing:
                 await ctx.invoke(self.resume)
                 return
             else:
-                await self.bot.say('Already playing a song')
+                await self.bot.say('Уже играю песенку.')
                 return
             
         while True:
@@ -114,12 +107,42 @@ class Radio:
                 headers = dict(self.copycom.session.headers))
             self.stopped = False
             self.player.start()
-            fmt = 'Playing song "{0}"'
             song_name = unquote(self.current.split('/')[-1])
-            await self.bot.say(fmt.format(song_name))
             await self.bot.change_status(discord.Game(name=song_name))
             
             await self.play_next_song.wait()
+            
+    @commands.command(aliases=['c'])
+    async def current():
+        """"Что там на радио?"""
+        if self.is_playing:
+            song_name = unquote(self.current.split('/')[-1])
+            await self.bot.say(song_name)
+        
+    @commands.command()
+    async def update():
+        """Обновить список песен."""
+        self.update_song_list()
+        await self.bot.say("Найдено {} песенок".format(len(self.files)))
+    
+    @commands.command()
+    async def list():
+        """Вывести список всех доступных песен."""
+        song_list = ""
+        id = 1
+        for song in self.songs:
+            song_list += "{}. {}\n".format(id, song)
+            id += 1
+            if len(song_list) > 1800:
+                await self.bot.say(song_list)
+                song_list = ''
+        await self.bot.say(song_list)
+        
+    @commands.command()
+    async def add(song_num : int):
+        """Добавить в конец очереди песню с данным номером."""
+        await self.q.put(self.songs[song_num-1])
+        await self.bot.say("{} будет следующей песенкой".format(self.songs[song_num-1]))
             
 def setup(bot):
     bot.add_cog(Radio(bot))
