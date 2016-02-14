15afd7c73a9732632bda5a7d6ee831976b5b14df
Haitaka <haitaka@users.noreply.github.com>
2016-02-14 17:11:07 +0000

    Update radio.py

--- cogs/radio.py
+++ cogs/radio.py
@@ -2,6 +2,8 @@ from .utils import config, checks, formats
 import discord
 from discord.ext import commands
 import discord.utils
+import .utils.api.pycopy as pycopy
+import random, json
 
 class Radio:
     """The radio-bot related commands."""
@@ -9,12 +11,32 @@ class Radio:
     def __init__(self, bot):
         self.bot = bot
         self.player = None
+        self.stopped = True
+        self.q = asyncio.Queue()
+        self.play_next_song = asyncio.Event()
+        self.current_song = None
+        copy_creds = self.load_copy_creds()
+        self.copycom = pycopy.Copy(copy_creds['login'], copy_creds['passwd'])
+        self.songs = []
+        self.update_song_list()
         if not discord.opus.is_loaded():
             discord.opus.load_opus('/usr/local/lib/libopus.so') #FreeBSD path
 
+    def load_copy_creds():
+        with open('../copy_creds.json') as f:
+            return json.load(f)
+            
     @property
     def is_playing(self):
-        return self.player is not None and self.player.is_playing()
+        return self.player is not None and self.player.is_playing() and not self.stopped
+        
+    def toggle_next_song(self):
+        if not self.stopped:
+            self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
+
+    def update_song_list(self):
+        self.files = self.copycom.list_files(settings.copy_radio_path)
+    
     
     @commands.command()
     async def join(self, *, channel : discord.Channel = None):
@@ -28,7 +50,7 @@ class Radio:
     async def leave(self):
         """Leave voice channel.
         """
-        await self.stop()
+        await self.stop().invoke(ctx)
         await self.bot.voice.disconnect()
         
     @commands.command()
@@ -52,6 +74,49 @@ class Radio:
         if self.player is not None and self.is_playing():
             self.player.stop()
             self.toggle_next_song()
+            
+    @commands.command()
+    async def stop():
+        """Stop playing song.
+        """
+        if self.is_playing():
+            self.stopped = True
+            self.player.stop()
 
+    @commands.command(pass_context=True)
+    async def play(self, ctx):
+        """Start playing song from queue.
+        """
+        if self.player is not None:
+            if not self.is_playing():
+                await self.resume().invoke(ctx)
+                return
+            else:
+                await self.bot.say('Already playing a song')
+                return
+            
+        while True:
+            if not selfbot.is_voice_connected():
+                await self.join(channel=ctx.message.author.voice_channel).invoke(ctx)
+                continue
+    
+            if self.q.empty():
+                await self.q.put(random.choice(self.songs))
+            self.play_next_song.clear()
+            self.current = await self.q.get()
+            self.player = self.bot.voice.create_ffmpeg_player(
+                self.copycom.direct_link(settings.copy_radio_path + self.current),
+                after=self.toggle_next_song,
+                #options="-loglevel debug -report",
+                headers = dict(self.copycom.session.headers))
+            self.stopped = False
+            self.player.start()
+            fmt = 'Playing song "{0}"'
+            song_name = unquote(self.current.split('/')[-1])
+            await bot.say(fmt.format(song_name))
+            self.bot.change_status(discord.Game(name=song_name))
+            
+            await self.play_next_song.wait()
+            
 def setup(bot):
     bot.add_cog(Radio(bot))
