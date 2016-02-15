c3df77b87d5f22925cd94a04e21b67507616219a
Haitaka <haitaka@users.noreply.github.com>
2016-02-15 11:16:38 +0000

    Update pictures.py

--- cogs/pictures.py
+++ cogs/pictures.py
@@ -8,24 +8,24 @@ class Pic:
     def __init__(self, bot):
         self.bot = bot
         self.pic_dir = 'pictures/'
-        self.pic_list = []
+        self.pic_dict = {}
         self.update_pics()
         
     def update_pics(self):
         file_list = self.bot.pycopy.list_files(self.pic_dir)
         for file_name in file_list:
-            self.pic_list[file_name.split('.')[0]] = file_name
-        self.pic.aliases = list(self.pic_list.values())
+            self.pic_dict[file_name.split('.')[0]] = file_name
+        self.pic.aliases = list(self.pic_dict.values())
     
     @commands.group(pass_context=True, aliases=[])
     async def pic(self, ctx):
         """База картинок, мемесов etc."""
-        if ctx.invoked_with in self.pic_list:
-            url = self.bot.pycopy.direct_link(self.pic_path + self.pic_list[ctx.invoked_with])
+        if ctx.invoked_with in self.pic_dict:
+            url = self.bot.pycopy.direct_link(self.pic_path + self.pic_dict[ctx.invoked_with])
             r = requests.get(url, stream=True)
             if r.status_code == 200:
                 r.raw.decode_content = True
-                await self.bot.upload(r.raw, self.pic_list[ctx.invoked_with])
+                await self.bot.upload(r.raw, self.pic_dict[ctx.invoked_with])
         elif ctx.invoked_subcommand is None:
             msg = copy.copy(ctx.message)
             msg.content = ctx.prefix + 'help pic'
@@ -35,14 +35,14 @@ class Pic:
     async def update(self):
         """Обновить список картиночек."""
         self.update_pics()
-        await self.bot.say("Найдено {} картиночек.".format(len(self.pic_list)))
+        await self.bot.say("Найдено {} картиночек.".format(len(self.pic_dict)))
         
     @pic.command()
     async def list(self):
         """Вывести список картиночек."""
         pic_list = ''
         id = 1
-        for pic in self.pic_list:
+        for pic in self.pic_dict:
             pic_list += "{}. {}\n".format(id, pic)
             id += 1
             if len(pic_list) > 1800:
