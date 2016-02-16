ce3e32a49b353d8d4f61facf79b1bccf5ce95a50
Haitaka <haitaka@users.noreply.github.com>
2016-02-16 07:06:05 +0000

    Update rng.py

--- cogs/rng.py
+++ cogs/rng.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from discord.ext import commands
 import random as rng
 import copy
@@ -7,14 +6,15 @@ import copy
 class RNG:
     """Utilities that provide pseudo-RNG."""
 
+    el_fractions=['ходоки', 'лорды', 'некрофаги', 'маги', 'хуители']
+
     def __init__(self, bot):
         self.bot = bot
-        self.el_pull = copy.copy(RNG.el_frections)
+        self.el_pull = copy.copy(RNG.el_fractions)
 
     @commands.command()
     async def random(self, minimum=0, maximum=100):
         """Выбрать случайное число в заданном диапазоне.
-
         Минимум должен быть меньше максимума, а максимум — меньше 1000.
         """
 
@@ -35,12 +35,21 @@ class RNG:
     #    ])
     #    await self.bot.say(lenny)
 
+    @commands.group(pass_context=True, aliases=[])
+    async def el(self, ctx):
+        r_answer = ''
+        for fract in self.el_pull:
+            str_answer += '{}\n'.format(fract)
+        await self.bot.say(str_answer)
 
+    @el.command(pass_context=True)
+    async def repull(self, ctx):
+        self.el_pull = copy.copy(RNG.el_fractions)
+        ctx.invoke(self.el)
 
     @commands.command(aliases=['выбери', 'вибери'])
     async def choose(self, *, choices : str):
         """Есть два стула...
-
         Варианты должны быть разделены с помощью `or` или `или`
         """
         choices_list = list()
