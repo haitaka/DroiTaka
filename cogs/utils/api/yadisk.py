6e7a8ba52a1502daa078f31dc20fc0c3a8534957
Alexey Glushko <haitaka@users.noreply.github.com>
2016-04-08 08:04:29 +0000

    Update yadisk.py

--- cogs/utils/api/yadisk.py
+++ cogs/utils/api/yadisk.py
@@ -36,9 +36,18 @@ class YaDisk(object):
 	def _post(self, url, data, *args, **kwargs):
 		return self.session.post(url, {'data': json.dumps(data), }, *args, **kwargs)
 
+
 	def list_files(self, dir_path):
 		file_list = []
+		res = self._get("https://cloud-api.yandex.net:443/v1/disk/resources", params={"path": dir_path,})
+		print(res.json())
+		for file in res.json()['_embedded']['items']:
+			if file['type'] == 'file':
+				file_list.append(file['name'])
 		return file_list
 
 	def direct_link(self, file_path):
-		return ""
+		response = self.session._get("https://cloud-api.yandex.net:443/v1/disk/resources/download", 
+		                             params={"path": file_path,})
+		return response.json()['href']
+		
